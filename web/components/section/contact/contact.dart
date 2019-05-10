4dc891d5a54736aad0ae429de094c1e5f9a3feef
Wilkin Novo <w@novo.ws>
2019-05-10 11:29:26 -0400

    +address +phone

--- web/components/section/contact/contact.dart
+++ web/components/section/contact/contact.dart
@@ -3,6 +3,8 @@ import '../../../framework/applicationBase.dart';
 class Contact extends ApplicationBase {
   Contact() : super('address');
   static Contact instance = new Contact();
+  String phone = '(929) 324-0887';
+  String address = '417 Irving Ave, Brooklyn NY 11237';
 
   @override
   void init() {
@@ -14,13 +16,21 @@ class Contact extends ApplicationBase {
         .element
         .querySelector('a.github')
         .setAttribute('href', 'https://github.com/novo-ws');
+    this
+        .element
+        .querySelector('a.phone')
+        .setAttribute('href', 'tel:9293240887');
   }
 
   @override
   String render() {
     return """
       <div class="text-center contact">
-                <h1 class="title"><i class="fa fa-envelope"></i> Contact </h1>
+                <h1 class="title"><i class="fa fa-phone"></i> Contact </h1>
+                <a class="phone" href=""> ${this.phone}</a><br/>
+                <address>
+                    ${this.address}
+                </address>
                 <a class="contact-page" href=""><i class="fas fa-id-card-alt" title="Contact Form"></i></a> <a class="github" href=""><i class="fab fa-github" title="Github"></i></a>
       </div>
     """;
